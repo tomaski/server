c32e34aa91e2ff1d70d08396c436f108bdf2ab57
Julius Härtl <jus@bitgrid.net>
2019-02-21 17:30:12 +0100

    Add bundled files
    Signed-off-by: Julius Härtl <jus@bitgrid.net>

---
+++ apps/files_sharing/js/dist/files_sharing.3.js
@@ -0,0 +1,24 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{13:function(e,n,o){"use strict";o.r(n);var i=o(18),u=o(26),l=o(16),r=o(27),a=o.n(r),c=o(28),s=o(19),d={name:"CollaborationView",components:{CollectionList:s.a}},p=o(51),f=Object(p.a)(d,function(){var e=this.$createElement;return(this._self._c||e)("collection-list",{attrs:{type:"files"}})},[],!1,null,null,null).exports;o.d(n,"Store",function(){return w}),o.d(n,"Vue",function(){return i.default}),o.d(n,"View",function(){return f});
+/*
+ * @copyright Copyright (c) 2019 Julius Härtl <jus@bitgrid.net>
+ *
+ * @author Julius Härtl <jus@bitgrid.net>
+ *
+ * @license GNU AGPL version 3 or any later version
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+const w=()=>new u.a.Store(s.b);i.default.prototype.t=t,i.default.component("PopoverMenu",l.PopoverMenu),i.default.directive("ClickOutside",a.a),i.default.directive("Tooltip",c.a),i.default.use(u.a)}}]);
+//# sourceMappingURL=files_sharing.3.js.map
